b26994bf21167310297ea537d3722df43fe5a91c
peoelena-cloud <peoelena@gmail.com>
2026-06-06 00:16:41 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const { importOperations, fetchReport, morningReport } = require('./tbank');
 const USER_IDS = [433916681, 325532225]; // Елена, Аркадий
 
 const BOT_TOKEN = "8410628068:AAGseZ3UI7elPdJi_p7BqAcwXtxQ_kILTSo";
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyuv9Yn94p6PCljw9SandPzOUeENKrk_-eWEkX06XQFTXw4HpdKFWOHmQxlLpPHX6sa/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyzLbOoKnvcwc4fg-40IS8mKE3Ob-pxG-M9C75h6DFpYRaFzFQMpNJkX0ukoMQFsbdj/exec";
 const ALLOWED = [433916681, 325532225];
 
 // ─── СПРАВОЧНИКИ ───────────────────────────────────────────────────────────────
